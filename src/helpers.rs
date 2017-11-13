d0c9a695bf8911655aa521376cbc4f2e214e6e1c
Nathan Ringo <remexre@gmail.com>
2017-11-13 16:35:59 -0600

    asdfasdf

--- src/helpers.rs
+++ src/helpers.rs
@@ -4,16 +4,19 @@ use monto::Value;
 use monto::common::messages::{Language, Product, ProductIdentifier, ProductName};
 use monto::service::messages::{ServiceError, ServiceNotice};
 
-pub fn simple_fn<E: Display, F: FnOnce(String) -> Result<Value, E>>(path: &str, mut products: Vec<Product>, lang: Language, f: F) -> (Result<Value, Vec<ServiceError>>, Vec<ServiceNotice>) {
+pub fn simple_fn<E: Display, F: FnOnce(String) -> Result<Value, E>>(
+    path: &str,
+    mut products: Vec<Product>,
+    lang: Language,
+    f: F,
+) -> (Result<Value, Vec<ServiceError>>, Vec<ServiceNotice>) {
     let idx = products.iter().position(|p| {
         p.name == ProductName::Source && p.language == lang && p.path == path
     });
 
     let r = if let Some(idx) = idx {
         match products.swap_remove(idx).value {
-            Value::String(src) => {
-                f(src).map_err(|e| ServiceError::Other(e.to_string()))
-            }
+            Value::String(src) => f(src).map_err(|e| ServiceError::Other(e.to_string())),
             _ => Err(ServiceError::Other("bad source product".to_string())),
         }
     } else {
@@ -23,12 +26,16 @@ pub fn simple_fn<E: Display, F: FnOnce(String) -> Result<Value, E>>(path: &str, 
             path: path.to_string(),
         }))
     };
-    let notices = products.into_iter()
+    let notices = products
+        .into_iter()
         .map(|p| p.into())
         .map(ServiceNotice::UnusedDependency)
         .collect();
-    (match r {
-        Ok(product) => Ok(product),
-        Err(err) => Err(vec![err]),
-    }, notices)
+    (
+        match r {
+            Ok(product) => Ok(product),
+            Err(err) => Err(vec![err]),
+        },
+        notices,
+    )
 }
